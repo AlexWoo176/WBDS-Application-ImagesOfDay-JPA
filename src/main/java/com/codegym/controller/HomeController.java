0fe04128f2d1827c9a779fbad81e2d4338b40b20
AlexWoo176 <phuong.tuan2012@gmail.com>
2020-06-04 15:07:15 +0700

    [Bài tập] Nâng cấp ứng dụng Bức ảnh của ngày

---
+++ src/main/java/com/codegym/controller/HomeController.java
@@ -0,0 +1,42 @@
+package com.codegym.controller;
+
+import com.codegym.model.Comment;
+import com.codegym.repository.CommentRepository;
+import com.codegym.service.CommentService;
+import com.codegym.service.impl.CommentServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    private CommentService commentService = new CommentServiceImpl();
+
+    @GetMapping("/")
+    public ModelAndView getHomePage(){
+        List<Comment> comments = commentService.getAllCommentByDate();
+        ModelAndView modelAndView = new ModelAndView("index","listComment",comments);
+        modelAndView.addObject("newComment", new Comment());
+        return modelAndView;
+    }
+    @PostMapping("/")
+    public String insertComment(@ModelAttribute Comment newComment){
+        commentService.insertComment(newComment);
+        return "redirect:/";
+    }
+    @GetMapping("/increment/{id}")
+    public String incrementLikes(@PathVariable long id){
+        Comment comment = commentService.getById(id);
+        comment.setLikes(comment.getLikes() + 1);
+        commentService.incrementLike(comment);
+        return "redirect:/";
+    }
+}
